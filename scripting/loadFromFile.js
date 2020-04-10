8b29aa323ae34dd4b8de199282c3952f841acc17
Cameron Falls <camfalls0201@gmail.com>
2020-04-09 20:01:07 -0500

    Fix file loading

--- scripting/loadFromFile.js
+++ scripting/loadFromFile.js
@@ -1,8 +1,10 @@
+const MY_URL = "https://fallscameron01.github.io/";
+
 function loadFromFile(element, filename)
 {
     var hash = "#".concat(element);
     var dot = ".".concat(element);
-    var url = ($(location).attr('href')).concat("/", filename);
+    var url = MY_URL.concat(filename);
     $(document).ready( function() {
         $(hash).ready( function() {
             $(dot).load(url);
